9b77bacc4a55fd3d55a7231a4acfe7dd6936d5a8
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-08 16:15:19 +0400

    feat: add array with films

---
+++ filmsArray.js
@@ -0,0 +1,82 @@
+export const filmsArray = [
+    {
+        id: 1,
+        imgUrl: 'assets/img/thumbs/avatar.jpg',
+        movieName: 'Avatar',
+        releaseYear: 2012,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 2,
+        imgUrl: 'assets/img/thumbs/beauty-and-the-beast.jpg',
+        movieName: 'Beauty and the beast',
+        releaseYear: 2009,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 3,
+        imgUrl: 'assets/img/thumbs/captain-marvel.jpg',
+        movieName: 'Captain marvel',
+        releaseYear: 2019,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 4,
+        imgUrl: 'assets/img/thumbs/finding-nemo.jpg',
+        movieName: 'Finding Nemo',
+        releaseYear: 2015,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 5,
+        imgUrl: 'assets/img/thumbs/gone-girl.jpg',
+        movieName: 'Gone girl',
+        releaseYear: 2008,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 6,
+        imgUrl: 'assets/img/thumbs/monsters-inc.jpg',
+        movieName: 'Monsters inc',
+        releaseYear: 2018,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 7,
+        imgUrl: 'assets/img/thumbs/star-wars.jpg',
+        movieName: 'Star wars',
+        releaseYear: 1979,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 8,
+        imgUrl: 'assets/img/thumbs/the-social-network.jpg',
+        movieName: 'Social network',
+        releaseYear: 2011,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 9,
+        imgUrl: 'assets/img/thumbs/titanic.jpg',
+        movieName: 'Titanic',
+        releaseYear: 2009,
+        isFavorite: false,
+        description: 'The best movie ever'
+    },
+    {
+        id: 10,
+        imgUrl: 'assets/img/thumbs/toy-story.jpg',
+        movieName: 'Toy story',
+        releaseYear: 2012,
+        isFavorite: false,
+        description: 'The best movie ever'
+    }
+]
