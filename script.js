30c9d41526d599f76a6ea4d38f7cc758002274a3
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-11-08 18:57:55 +0400

    feat: add rendering favorite films

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { filmsArray } from "./filmsArray.js";
 import { ALL_FILMS } from "./assets/js/constants.js";
+import { FAVORITE_FILMS } from "./assets/js/constants.js";
 import { fromStorage } from "./assets/js/fromStorage.js";
 import { toStorage } from "./assets/js/toStorage.js";
 import { renderFilmCard } from "./assets/js/renderFilmCard.js";
@@ -9,6 +10,9 @@ if (!fromStorage(ALL_FILMS)) {
 }
 
 renderFilmsList(fromStorage(ALL_FILMS), ALL_FILMS)
+
+const favoriteFilmsButton = document.querySelector('.film-cards-container-favorite-films')
+favoriteFilmsButton.addEventListener('click', () => handleFilmListSwitch(favoriteFilmsButton))
 
 function renderFilmsList(filmsList, listType) {
     const favoriteFilmsButton = document.querySelector('.film-cards-container-favorite-films')
@@ -20,4 +24,19 @@ function renderFilmsList(filmsList, listType) {
     const filmsContainerHTML = document.querySelector('.film-cards-container')
 
     filmsList.forEach((film, i) => renderFilmCard(film, filmsContainerHTML, i))
+}
+
+function handleFilmListSwitch(switchButton) {
+    const filmsContainerHTML = document.querySelector('.film-cards-container')
+    const filmsCardContainerTitle = document.querySelector('.film-cards-container-title')
+
+    const favoriteFilms = fromStorage(ALL_FILMS).filter(({ isFavorite }) => isFavorite)
+
+    switch (filmsContainerHTML.id) {
+        case ALL_FILMS:
+            filmsCardContainerTitle.innerText = 'Favorite Films'
+            switchButton.innerText = 'Click to See All Films'
+            filmsContainerHTML.remove()
+            renderFilmsList(favoriteFilms, FAVORITE_FILMS)
+    }
 }
